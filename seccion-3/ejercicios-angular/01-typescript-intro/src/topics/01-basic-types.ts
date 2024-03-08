4de547c8fbf9d5e7e718c962091db3dcdac6ac2a
rodrisolisavila <rodri.solis.avila@gmail.com>
2024-03-07 19:22:39 -0600

    initial commit

---
+++ seccion-3/ejercicios-angular/01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,13 @@
+//let hpPoints: number | string = 150;
+//let hpPoints: number | 'Full' = 150;
+//hpPoints = 'Full';
+
+const name = 'Strider';
+let hpPoints: number = 150;
+const isAlive:boolean = true;
+
+console.log({
+    name,hpPoints,isAlive
+})
+
+export{}
